99592bcf0b26b0479d487207b22b5019b1baaab4
KabirSinghShekhawat <kabirsinghshekhawat1789@gmail.com>
2022-11-25 15:56:20 +0530

    refactor(log route): remove redundant logging route

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from sse_starlette.sse import EventSourceResponse
 from fastapi import FastAPI, Request
 import uvicorn
 from app.handlers import log_handler
-from app.utils import log_maker
 
 app = FastAPI()
 
@@ -11,11 +10,6 @@ app = FastAPI()
 async def read_logs(id: str, request: Request):
     event = log_handler.read_logs(request)
     return EventSourceResponse(event)
-
-
-# @app.get("/generate_logs/{lines}")
-# async def make_logs(lines: int):
-#     await log_maker.run(lines)
 
 
 if __name__ == "__main__":
